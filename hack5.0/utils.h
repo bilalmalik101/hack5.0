4ee61087256cd090c94a78a1744a41e51ff8ac27
bilalmalik101 <144342543+bilalmalik101@users.noreply.github.com>
2023-12-21 00:07:32 +0500

    add utils header for convert degree into radian

---
+++ hack5.0/utils.h
@@ -0,0 +1,6 @@
+//prototype of getAirDistance function for header
+double getAirDistance(double originLatitude,double originLongitude,
+double destinationLatitude,double destinationLongitude);
+//Prototypes of degreeToRadians function
+double degreeToRadians(double degree);
+double lorentzTimeDilation(double t,double percentC);
