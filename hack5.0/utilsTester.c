9bdaaf262d8f84608d69a039a3659e2b3083e9c3
bilalmalik101 <144342543+bilalmalik101@users.noreply.github.com>
2023-12-21 12:10:12 +0500

    add isclose function and test case condition

--- hack5.0/utilsTester.c
+++ hack5.0/utilsTester.c
@@ -11,5 +11,16 @@ int main(){
     int failed=0;
     int passed=0;
 //test case for degree to radian function
+double latitude=50.0;
+double expected=0.872665;
+double returnedvalue=(degreeToRadians(latitude));
+if(isclose(returnedvalue,expected)){
+    printf("passed\n");
+    passed++;
+}
+else{
+    printf("failed: degreetoradians returned %1f, expected %1f\n",returnedvalue,expected);
+    failed++;
+}
 
 }
